c8b9c21ab455b918013baaf4e7cf25da09fc8403
Mohd Shahnawaz <32464723+shahnawaz8@users.noreply.github.com>
2022-03-03 23:44:27 +0530

    final

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var os = ["Android","Blackberry","iPhone","Windows Phone"];
+  var mf = ["Samsung","HTC","Micromax","Apple"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Mobile Operating System</h1>
+        <ul>
+          {
+          os.map((i)=>
+            <li>{i}</li>
+          )
+          }
+        </ul>
+        <h1>Mobile Manufacturers</h1>
+        <ul>
+          {/* { mf.map((i)=>
+            <li>{i}</li>
+          )} */
+          }
+          <li className="clist1">Samsung</li>
+          <li className="clist1">HTC</li>
+          <li>Micromax</li>
+          <li className="clist">Apple</li>
+
+          
+        </ul>
     </div>
   );
 }
